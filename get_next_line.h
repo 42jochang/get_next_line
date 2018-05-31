00b5b0fd533624f62ad2108b589cc2231e743e4a
Jo Chang <jochang@e1z2r4p11.42.us.org>
2018-05-31 02:50:44 -0700

    initial upload

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jochang <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/25 17:56:34 by jochang           #+#    #+#             */
+/*   Updated: 2018/05/30 22:19:54 by jochang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 42
+# define ERROR_CHECK(x) if (x) return (-1)
+# define BREAK_CHECK(x) if (x) break
+
+# include "libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
